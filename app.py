b94c0024bd078d81ca493cb9c72a3aff2f7ee544
JuicyOatmeal <felix.d.g.royle@gmail.com>
2024-05-06 12:26:41 +1200

    Added html files, started on getting databases into app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error, connect
 
 app = Flask(__name__)
+database = "databases.db"
+
+
+def get_database():
+    try:
+        returned_db = sqlite3.connect(database)
+        return returned_db
+    except Error as error:
+        print(error)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home():
+    con = connect(database)
+
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
